5aaf2d2f254270d5d56bbc79fad8c5dddd747ece
EnoughNthani <enoughnthani@gmail.com>
2026-04-19 19:44:34 +0200

    updates

--- src/main/java/com/app/b_and_t_lms/controllers/HomeController.java
+++ src/main/java/com/app/b_and_t_lms/controllers/HomeController.java
@@ -1,9 +1,6 @@
 package com.app.b_and_t_lms.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.app.b_and_t_lms.repositories.UserOtpRepository;
 
 
 @RestController
